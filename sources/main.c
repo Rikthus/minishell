e93b946c5b364e02d9d0bbae49ad7e7e6abfbcfc
maxperei <maxperei@student.42lyon.fr>
2022-09-22 19:08:39 +0200

    Add history of commands

--- sources/main.c
+++ sources/main.c
@@ -6,36 +6,36 @@
 /*   By: tulipe <tulipe@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 17:52:00 by maxperei          #+#    #+#             */
-/*   Updated: 2022/09/21 19:05:06 by tulipe           ###   ########lyon.fr   */
+/*   Updated: 2022/09/22 19:07:46 by tulipe           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-// static	void	print_tokens(t_token *token)
-// {
-// 	int	i;
-// 	int	j;
+static	void	print_tokens(t_token *token)
+{
+	int	i;
+	int	j;
 
-// 	while (token)
-// 	{
-// 		i = 0;
-// 		j = 0;
-// 		while (token->cmd[i])
-// 		{
-// 			printf("%s\n", token->cmd[i]);
-// 			i++;
-// 		}
-// 		printf("\n");
-// 		while (token->target[j])
-// 		{
-// 			printf("%d   %s\n", token->redir[j], token->target[j]);
-// 			j++;
-// 		}
-// 		printf("\n\n\n");
-// 		token = token->next;
-// 	}
-// }
+	while (token)
+	{
+		i = 0;
+		j = 0;
+		while (token->cmd[i])
+		{
+			printf("%s\n", token->cmd[i]);
+			i++;
+		}
+		printf("\n");
+		while (token->target[j])
+		{
+			printf("%d   %s\n", token->redir[j], token->target[j]);
+			j++;
+		}
+		printf("\n\n\n");
+		token = token->next;
+	}
+}
 
 // static	void	print_env(t_envlist *env_list)
 // {
@@ -51,7 +51,11 @@ static	int	controler(char *raw_line, t_envlist *env_list)
 	t_base	*basic_token;
 	t_token	*token;
 
-	if (!raw_line || raw_line[0] == '\0' || !pre_parsing(raw_line))
+	(void)env_list;
+	if (!raw_line || raw_line[0] == '\0')
+		return (free_rd_line(raw_line, BAD_EXIT));
+	add_history(raw_line);
+	if (!pre_parsing(raw_line))
 		return (free_rd_line(raw_line, BAD_EXIT));
 	basic_token = tokenizer(raw_line);
 	if (!basic_token)
@@ -60,12 +64,12 @@ static	int	controler(char *raw_line, t_envlist *env_list)
 	free_basic_token(basic_token);
 	if (!token)
 		return (0);
-	// print_tokens(token);
-	if (!expander(&token, env_list))
-	{
-		free_token(token);
-		return (free_rd_line(raw_line, BAD_EXIT));
-	}
+	print_tokens(token);
+	// if (!expander(&token, env_list))
+	// {
+	// 	free_token(token);
+	// 	return (free_rd_line(raw_line, BAD_EXIT));
+	// }
 	// redir & exec
 	free_token(token);
 	return (free_rd_line(raw_line, GOOD_EXIT));
