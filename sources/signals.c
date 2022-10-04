c24b6ad5e45cdac3a7e154f56d155f0a552a7d7f
maxperei <maxperei@student.42lyon.fr>
2022-10-04 14:58:04 +0200

    Fix signals

--- sources/signals.c
+++ sources/signals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maxperei <maxperei@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: tulipe <tulipe@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 20:54:14 by tulipe            #+#    #+#             */
-/*   Updated: 2022/10/03 18:45:56 by maxperei         ###   ########lyon.fr   */
+/*   Updated: 2022/10/04 14:57:19 by tulipe           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,27 +20,36 @@ static	void	handle_shell(int signum)
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
+		g_shell.exit_status = 130;
 	}
 }
 
 static	void	handle_exec(int signum)
 {
 	if (signum == SIGINT)
+	{
 		ft_putstr_fd("\n", 1);
+		g_shell.exit_status = 130;
+	}
 	else
+	{
 		ft_putstr_fd("Quit: 3\n", 1);
+		g_shell.exit_status = 131;
+	}
 }
 
 static	void	handle_pstop(int signum)
 {
 	(void)signum;
 	g_shell.herestop = 1;
+	g_shell.exit_status = 1;
 }
 
 static	void	handle_heredoc(int signum)
 {
 	(void)signum;
 	g_shell.herestop = 1;
+	g_shell.exit_status = 1;
 	close(0);
 	ft_putstr_fd("\n", 1);
 }
